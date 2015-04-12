14a116c4d88b5882285219dbd0348c91c7dbb204
gleuch <greg@betaworks.com>
2015-04-12 00:58:48 -0400

    deploy fix.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :format, :pretty
 set :log_level, :debug
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
 
